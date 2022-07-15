67578eac56e5a52278e14ca1982528b1d0b3d4b0
vincaf <vincenzocafassog@gmail.com>
2022-07-15 16:18:48 +0200

    created header, main and footer components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderWeb />
+    <MainWeb />
+    <FooterWeb />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderWeb from './components/HeaderWeb.vue';
+import MainWeb from './components/MainWeb.vue';
+import FooterWeb from './components/FooterWeb.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderWeb,
+    MainWeb,
+    FooterWeb,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import "~bootstrap/scss/bootstrap.scss";
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
